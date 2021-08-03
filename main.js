dc9dd293c8b735d370be9f7b0d5d86c64729ff7c
Filipe Melo <filipexpert63@gmail.com>
2021-08-03 15:40:05 -0300

    Finalização da parte de lógica!

--- main.js
+++ main.js
@@ -4,7 +4,6 @@ let towerThree = document.getElementById('tower-three')
 
 const box = document.querySelectorAll(".container")
 
-//class transformar
 
 for(let i = 1; i <= 4; i++){
     let newDiv = document.createElement('div')
@@ -18,34 +17,65 @@ for(let i = 1; i <= 4; i++){
 let firstClick = ""
 let secondClick = ""
 let validate = false;
-// função para retirar os filhos do bloco
-
-
-const start = (event) => {
-
-    let call = event.currentTarget;
-    
-    if(validate === false) {
-
-        firstClick = call.querySelector(".box");
-        validate = true;
-    }
-    else {
-        
-        secondClick = call.querySelector(".box");
-        validate = false;
-    }
-}
 
 for(let i = 0; i < box.length; i++) {
 
-    box[i].addEventListener("click", start);
+    box[i].addEventListener("click",function(event) {
+
+        const call = event.currentTarget;
+        
+        if(validate === false) {
+
+            firstClick = call.querySelector(".box");
+            call.classList.add("transformar")
+            validate = true;
+        }
+        else {
+            
+            secondClick = call.querySelector(".box");
+
+            let arrFirst = tratamentoDeArray(firstClick)
+            let arrSecond = tratamentoDeArray(secondClick)
+            let condicao = condicaoTroca(arrFirst, arrSecond);
+            if(condicao === true) {
+                mudarBox(firstClick, secondClick);
+            }
+
+            let elementClass = document.querySelector(".transformar")
+            elementClass.classList.remove("transformar")
+
+            let towerTwoArr = tratamentoDeArray(box[1])
+            let towerThreeArr = tratamentoDeArray(box[2])
+
+            if (verificaVitoria(towerTwoArr) === true || verificaVitoria(towerThreeArr) === true ) {
+                console.log('winner')
+            }
+
+            firstClick = ""
+            validate = false;
+        }
+    });
+
 }
 
+
+
 const condicaoTroca = (firstClick, secondClick) => {
-    // Criar uma função para chamar aqui dentro
-    // que tranforme o valor de first e second em numeros
+    let item = firstClick[firstClick.length - 1]; 
+    let lastClick =secondClick[secondClick.length - 1] 
+    
+    if ( item !== undefined ) {
+        
+        if ( item > lastClick || lastClick === undefined ) {
+            return true
+        }
+        
+    }
+    
+    return false
+
 }
+
 
 
 function tratamentoDeArray(value){
@@ -53,11 +83,12 @@ function tratamentoDeArray(value){
     const arr = value.getElementsByClassName('disco')
     let arr2 = []
 
-    if(arr[0] === undefined) {
-        return arr2;
-    }
+    
     
     for ( let i = 0; i < 4; i++ ) {
+        if(arr[i] === undefined) {
+            return arr2;
+        }
             let a = arr[i].id;
 
             arr2.push(Number(a[1]))
@@ -66,19 +97,14 @@ function tratamentoDeArray(value){
     return arr2;
 }
 
+const mudarBox = (firstClick, secondClick) => {
+    secondClick.appendChild( firstClick.lastElementChild )
+}
 
-
-
-        // let arrFirst = tratamentoDeArray(firstClick)
-        // let arrSecond = tratamentoDeArray(secondClick)
-        
-
-        // console(arrFirst, arrSecond)
-        // let condicao = condicaoTroca(arrFirst, arrSecond);
-        // console.log(condicao)
-        // if(condicao === true) {
-        //     mudarBox(firstClick, secondClick);
-        // }
-       
-        // console.log(firstClick, secondClick);
-        // Criar função de condição de troca;y
+function verificaVitoria(valorArr){
+    const teste = [1, 2, 3, 4]
+    if( valorArr.join() === teste.join() ){
+        return true
+    }
+    return false
+}
